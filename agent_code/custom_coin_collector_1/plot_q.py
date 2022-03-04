27d6e31de6db28f6680c7c55a48b709f527548a0
rr3072 <johannes.maul@gmx.de>
2022-03-04 15:37:29 +0100

    modelUpdates

--- agent_code/custom_coin_collector_1/plot_q.py
+++ agent_code/custom_coin_collector_1/plot_q.py
@@ -5,9 +5,11 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    with open("model.pt", "rb") as file:
+    with open("models/model2500+3-5.pt", "rb") as file:
         Q = pickle.load(file)
 
     plt.gray()
     plt.imshow(np.reshape(Q, (9 * 27, 4)))
-    plt.show()
+    plt.savefig("q.svg")
+    #plt.show()
+
