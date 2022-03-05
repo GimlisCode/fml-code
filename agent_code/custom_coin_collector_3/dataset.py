24fbde81d13a6a6b07ed7eeead1bbcc7a4b326e2
lukas.klinger <lukas.klinger@kit.edu>
2022-03-05 21:34:52 +0100

    added DQN like idea

---
+++ agent_code/custom_coin_collector_3/dataset.py
@@ -0,0 +1,96 @@
+import json
+from typing import Tuple, List
+
+from torch.utils.data import Dataset
+from torch.utils.data.dataset import T_co
+import torch
+
+
+class TrainDataPoint:
+    def __init__(self, state_features_t, action, reward, state_features_t_plus_1):
+        self.state_features_t = state_features_t
+        self.action = action
+        self.reward = reward
+        self.state_features_t_plus_1 = state_features_t_plus_1
+
+    def __eq__(self, other):
+        if hash(self) == hash(other):
+            return True
+        return False
+
+    def __hash__(self):
+        return hash((tuple(self.state_features_t), self.action, self.reward, tuple(self.state_features_t_plus_1)))
+
+
+class GameStateDataset(Dataset):
+    def __init__(self, state_features_t, actions, rewards, state_features_t_plus_1):
+        if (
+                len(actions) != len(state_features_t)
+                or len(actions) != len(rewards)
+                or len(actions) != len(state_features_t_plus_1)
+        ):
+            raise ValueError("Mismatch in dataset size")
+
+        # self.state_features_t = state_features_t
+        # self.actions = actions
+        # self.rewards = rewards
+        # self.state_features_t_plus_1 = state_features_t_plus_1
+
+        self.data = set()
+        for idx in range(len(actions)):
+            self.data.add(TrainDataPoint(state_features_t[idx],
+                                         actions[idx],
+                                         rewards[idx],
+                                         state_features_t_plus_1[idx]))
+
+        self.data = list(self.data)
+
+    @staticmethod
+    def from_file(file: str) -> 'GameStateDataset':
+        with open(file, "r") as f:
+            train_data = json.loads(f.read())
+
+        return GameStateDataset(train_data["state_features_t"],
+                                train_data["action"],
+                                train_data["reward"],
+                                train_data["state_features_{t+1}"])
+
+    @staticmethod
+    def from_dict(train_data) -> 'GameStateDataset':
+        return GameStateDataset(train_data["state_features_t"],
+                                train_data["action"],
+                                train_data["reward"],
+                                train_data["state_features_{t+1}"])
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, index: int) -> T_co:
+        # feat_t = torch.tensor(self.state_features_t[index]).double()
+        # action = self.actions[index]
+        # reward = self.rewards[index]
+        # feat_t_plus_1 = torch.tensor(self.state_features_t_plus_1[index]).double()
+
+        feat_t = torch.tensor(self.data[index].state_features_t).double()
+        action = self.data[index].action
+        reward = self.data[index].reward
+        feat_t_plus_1 = torch.tensor(self.data[index].state_features_t_plus_1).double()
+
+        return feat_t, action, reward, feat_t_plus_1
+
+    def to_dict(self):
+        data = dict()
+
+        data["state_features_t"] = list()
+        data["action"] = list()
+        data["reward"] = list()
+        data["state_features_{t+1}"] = list()
+
+        for data_point in self.data:
+            data["state_features_t"] .append(data_point.state_features_t)
+            data["action"].append(data_point.action)
+            data["reward"].append(data_point.reward)
+            data["state_features_{t+1}"].append(data_point.state_features_t_plus_1)
+
+        return data
+
---
+++ agent_code/custom_coin_collector_3/train.py
@@ -0,0 +1,88 @@
+from typing import List
+
+from torch.utils.data import DataLoader
+
+import events as e
+from .callbacks import *
+
+from .dataset import GameStateDataset
+
+
+def setup_training(self):
+    self.Q.learning_rate = 0.001
+    self.Q.gamma = 0.25
+
+    self.optimizer = self.Q.configure_optimizers()
+
+
+def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_state: dict, events: List[str]):
+    if old_game_state is None:
+        return
+
+    if len(np.array(new_game_state["coins"])) == 0:
+        return
+
+    self.train_data["reward"].append(calculate_reward(events, old_game_state, new_game_state))
+    self.train_data["state_features_t"].append(state_to_features(old_game_state).tolist())
+    self.train_data["state_features_{t+1}"].append(state_to_features(new_game_state).tolist())
+    self.train_data["action"].append(get_idx_for_action(self_action))
+
+
+def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
+    dataset = GameStateDataset.from_dict(self.train_data)
+    train_loader = DataLoader(dataset, batch_size=20, shuffle=True)
+
+    self.Q.train()
+
+    for i, data in enumerate(train_loader, 0):
+        state_features_t, action, reward, state_features_t_plus_1 = data[0].to(self.device), \
+                                                                    data[1].to(self.device), \
+                                                                    data[2].to(self.device), \
+                                                                    data[3].to(self.device)
+
+        self.optimizer.zero_grad()
+        loss = self.Q.training_step((state_features_t, action, reward, state_features_t_plus_1), 0)
+        loss.backward()
+        self.optimizer.step()
+
+        self.train_info["train_loss"].append(loss.item())
+
+    self.Q.eval()
+    self.Q.save("model.pt")
+
+    if len(self.train_data["action"]) > len(dataset):
+        # remove double entries
+        # print(f"{len(self.train_data['action'])} > {len(dataset)}")
+        del self.train_data
+        self.train_data = dataset.to_dict()
+
+    del dataset
+    del train_loader
+
+    with open("train_info.json", "w") as file:
+        file.write(json.dumps(self.train_info))
+
+    with open("train_data.json", "w") as file:
+        file.write(json.dumps(self.train_data))
+
+
+def calculate_reward(events, old_game_state, new_game_state) -> int:
+    game_rewards = {
+        e.COIN_COLLECTED: 1,
+        e.INVALID_ACTION: -1
+
+    }
+    reward_sum = 0
+    for event in events:
+        if event in game_rewards:
+            reward_sum += game_rewards[event]
+
+    previous_min_dist = np.min(get_steps_between(np.array(old_game_state["self"][3]), np.array(old_game_state["coins"])))
+    current_min_dist = np.min(get_steps_between(np.array(new_game_state["self"][3]), np.array(new_game_state["coins"])))
+
+    if current_min_dist < previous_min_dist:
+        reward_sum += 0.4
+    else:
+        reward_sum -= 0.5
+
+    return np.clip(reward_sum, a_min=-1, a_max=1).item()
