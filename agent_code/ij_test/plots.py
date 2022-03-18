a67e939f3b7648f5f7f4e4875d171ed0879fdfae
lukas.klinger <lukas.klinger@kit.edu>
2022-03-18 12:30:37 +0100

    updated test script

--- agent_code/ij_test/plots.py
+++ agent_code/ij_test/plots.py
@@ -1,19 +1,101 @@
 import json
+from typing import List, Dict, Any
+import itertools
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_num_of_negative_rewards(rewards_per_episode: list):
-    percentage_of_negative_rewards_per_episode = list()
+def load_agent_performance(path: str):
+    with open(path, "r") as f:
+        return json.loads(f.read())
 
-    for episode in rewards_per_episode:
-        percentage_of_negative_rewards_per_episode.append(np.mean(np.array(episode) <= 0))
 
-    plt.boxplot(percentage_of_negative_rewards_per_episode)
+def plot_num_of_negative_rewards(rewards_per_episode: List[List[int]], labels: List[str]):
+    for experiment_idx, current_rewards_per_episode in enumerate(rewards_per_episode):
+        percentage_of_negative_rewards_per_episode = list()
+
+        for episode in current_rewards_per_episode:
+            percentage_of_negative_rewards_per_episode.append(np.mean(np.array(episode) < 0))
+
+        plt.boxplot(percentage_of_negative_rewards_per_episode)
+
     plt.show()
 
 
+def plot_points(points_per_epoch: List[List[int]], labels: List[str]):
+    for experiment_idx, current_points_per_episode in enumerate(points_per_epoch):
+        plt.boxplot(current_points_per_episode, label=labels[experiment_idx])
+
+    plt.show()
+
+
+def to_latex_table(
+        experiment_performances: List[Dict[str, List[Any]]],
+        names: List[str],
+        caption: str,
+        label: str,
+        out_file: str
+):
+    output_stream = open(out_file, "w")
+
+    """
+    \begin{table}
+        \centering
+        \caption{Comparison of different agent implementations. The points were measured during playing the \ac{ch} scenario and the classic Bomberman scenario versus one coin\_collector enemy and averaged over five rounds. To avoid. In order to avoid long numbers, these have been rounded off. Just the latest versions of the different implementations are considered. Deep Reinforcement Learning implementations are not considered in this overview. Points \ac{ch}: Amount of collected coins (our agent:enemy agent); Points classic: amount of points (our agent:enemy agent); \# training episodes: number of episodes till the model is fully trained for both scenarios. }
+        \begin{tabular}{c|l|l|l}
+            \label{tab:OverviewTraining}
+            \textbf{Implementation} & \textbf{Points CH} & \textbf{Points classic} & \textbf{\# training episodes} \\ 
+            \hline
+            Coin-Collector 1 & 25:25 & - & ca. 1100 \\ 
+            Coin-Collector 2 & 25:25 & - & ca. 200 \\
+            Crate-Destroyer 1 & 25:25 & 5:4 & ca. 1500 \\
+            Crate-Destroyer 2 & 25:25 & 5:4 & ca. 2200 \\
+            Crate-Destroyer 3 & 25:25 & 5:4 & ca. 2200 \\
+            Indiana-Jones 1 & 25:25 & 7:4 & ca. 10$\times$2000 \\
+            \hline
+        \end{tabular}
+    \end{table}
+    """
+
+    print("\\begin{table}", file=output_stream)
+    print("\t\\centering", file=output_stream)
+    print(f"\t\\caption{{{caption}}}", file=output_stream)
+    print("\t\\begin{tabular}{c|l|l|l}", file=output_stream)
+    print(f"\t\t\\label{{{label}}}", file=output_stream)
+    print("\t\t\\textbf{Experiment} & \\textbf{Mean Reward} & \\textbf{Mean Points} & \\textbf{Deaths (\%)} \\\\ ",
+          file=output_stream)
+    print("\t\t\\hline", file=output_stream)
+
+    for experiment_idx,  experiment_performance in enumerate(experiment_performances):
+        mean_reward = round(np.mean(list(itertools.chain(*experiment_performance["rewards"]))).item(), 2)
+        mean_points = round(np.mean(experiment_performance["points"]).item(), 2)
+        death_percentage = round(np.mean(experiment_performance["agent_died"]) * 100, 2)
+
+        print(f"\t\t{names[experiment_idx]} & {mean_reward} & {mean_points} & {death_percentage} \\\\",
+              file=output_stream)
+
+    print("\t\t\\hline", file=output_stream)
+    print("\t\\end{tabular}", file=output_stream)
+    print("\\end{table}", file=output_stream)
+
+    output_stream.close()
+
+
 if __name__ == '__main__':
-    with open("rewards.json", "r") as f:
-        plot_num_of_negative_rewards(json.loads(f.read())["rewards"])
+    experiments = {
+        # "no improvements": "../../ExperimentData/noAugNoPar/performance.json",
+        # "augmentations": "../../ExperimentData/augmentation/performance.json",
+        # "parallel": "../../ExperimentData/parallel(noAug)/performance.json",
+        "parallel and augmentation": "../../ExperimentData/parallel+augmentation/performance.json"
+    }
+
+    experiment_performances = list()
+
+    for experiment_key in experiments:
+        experiment_performances.append(load_agent_performance(experiments[experiment_key]))
+
+    plot_num_of_negative_rewards([performance["rewards"] for performance in experiment_performances],
+                                 list(experiments.keys()))
+
+    to_latex_table(experiment_performances, list(experiments.keys()), "Test", "tab:experiments_comparison", "test.tex")
--- agent_code/ij_test/train.py
+++ agent_code/ij_test/train.py
@@ -1,10 +1,13 @@
 from typing import List
-from pathlib import Path
 import json
+
+import numpy as np
+
+from agent_code.ij_1.train import calculate_reward
+from agent_code.ij_1.callbacks import map_game_state_to_image, get_idx_for_action
 
 
 import events as e
-from .callbacks import *
 
 
 def setup_training(self):
@@ -46,93 +49,3 @@ def end_of_round(self, last_game_state: dict, last_action: str, events: List[str
                 "points": self.current_points,
                 "agent_died": self.agent_died
             }))
-
-
-def calculate_reward(events, old_game_state, new_game_state) -> int:
-    game_rewards = {
-        e.INVALID_ACTION: -2,
-        e.COIN_COLLECTED: 4
-    }
-    reward_sum = 0
-    for event in events:
-        if event in game_rewards:
-            reward_sum += game_rewards[event]
-
-    previous_agent_position = np.array(old_game_state["self"][3])
-    current_agent_position = np.array(new_game_state["self"][3])
-
-    previous_crate_positions = get_crate_positions(old_game_state)
-    current_crate_positions = get_crate_positions(new_game_state)
-
-    previous_coin_positions = old_game_state["coins"]
-    current_coin_positions = new_game_state["coins"]
-
-    previous_map = map_game_state_to_image(old_game_state)
-    current_map = map_game_state_to_image(new_game_state)
-
-    previous_other_agent_positions = get_other_agent_positions(old_game_state)
-    current_other_agent_positions = get_other_agent_positions(new_game_state)
-
-    previous_crate_direction, previous_crate_distance = find_next_crate(previous_map, previous_agent_position,
-                                                                        previous_crate_positions)
-    current_crate_direction, current_crate_distance = find_next_crate(current_map, current_agent_position,
-                                                                      current_crate_positions)
-
-    previous_coin_direction, previous_coin_distance = find_next_coin(previous_map, previous_agent_position,
-                                                                     previous_coin_positions)
-    current_coin_direction, current_coin_distance = find_next_coin(current_map, current_agent_position,
-                                                                   current_coin_positions)
-
-    previous_safe_field_direction, previous_safe_field_distance = find_next_safe_field(previous_map,
-                                                                                       previous_agent_position)
-    current_safe_field_direction, current_safe_field_distance = find_next_safe_field(current_map,
-                                                                                     current_agent_position)
-
-    previous_other_agent_direction, previous_other_agent_distance = find_next_agent(previous_map,
-                                                                                    previous_agent_position,
-                                                                                    previous_other_agent_positions)
-    current_other_agent_direction, current_other_agent_distance = find_next_agent(current_map,
-                                                                                  current_agent_position,
-                                                                                  current_other_agent_positions)
-
-    # --- BOMB DROP ---
-    next_to_crate = previous_crate_direction == CrateDirection.NEXT_TO
-    in_bomb_range = previous_other_agent_direction == NearestAgentDirection.IN_BOMB_RANGE
-
-    if e.BOMB_DROPPED in events and next_to_crate:
-        # AGENT DROPPED A BOMB AND WAS NEXT TO A CRATE
-        reward_sum += 1
-    if e.BOMB_DROPPED in events and in_bomb_range:
-        # AGENT DROPPED A BOMB AND ANOTHER AGENT WAS IN BOMB RANGE
-        reward_sum += 1
-    if e.BOMB_DROPPED in events and not next_to_crate and not in_bomb_range:
-        # AGENT DROPPED A BOMB BUT THERE WAS NO TARGET
-        reward_sum -= 5
-
-    # --- BOMB DODGE ---
-    if previous_safe_field_distance > current_safe_field_distance and current_safe_field_direction != SafeFieldDirection.UNREACHABLE:
-        # AGENT MOVED CLOSER TO SAFE FIELD
-        reward_sum += 5
-    elif current_safe_field_direction != SafeFieldDirection.IS_AT and e.BOMB_DROPPED not in events:
-        # AGENT DID NOT MOVE CLOSER TO SAFE FIELD AND IS NOT AT SAFE FIELD
-        reward_sum -= 18  # as the other positive reward can at most sum up to 9 but combined with alpha 0,5 the
-        # punishment must be greater than 2 * 9 such that it is not less than 9
-
-    agent_moved = not (e.BOMB_DROPPED in events or e.WAITED in events or e.INVALID_ACTION in events)
-
-    # --- COINS ---
-    if previous_coin_distance > current_coin_distance and not np.isinf(previous_coin_distance) and agent_moved:
-        # AGENT MOVED CLOSER TO COIN
-        reward_sum += 4
-
-    # --- CRATES ---
-    if previous_crate_distance > current_crate_distance and agent_moved:
-        # AGENT MOVED CLOSER TO CRATE
-        reward_sum += 3
-
-    # --- OTHER AGENTS ---
-    if previous_other_agent_distance > current_other_agent_distance and agent_moved:
-        # AGENT MOVED CLOSER TO THE NEAREST OTHER AGENT
-        reward_sum += 2
-
-    return reward_sum
